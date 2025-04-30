2c30dab6f2570cc7bddce7d070e3dc9d73f0f297
abdunzelil <mstfocak123@gmail.com>
2025-04-30 13:32:13 +0300

    Add aggressive cache control

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,12 @@ export default defineConfig({
         assetFileNames: `assets/[name]-[hash].[ext]`
       }
     }
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
   }
 });
